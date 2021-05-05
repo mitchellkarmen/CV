04eaf906763c7da5daddc2645dcd1d7eefc066c1
mitchellkarmen <mitchkarmen@yahoo.com>
2021-05-05 14:30:50 -0400

    removing suspicious apostrophes

--- .ci/build-cv.sh
+++ .ci/build-cv.sh
@@ -2,12 +2,12 @@
 
 #copied from https://dfm.io/posts/travis-latex/ + changes
 
-if git diff --name-only $`TRAVIS_COMMIT_RANGE
+if git diff --name-only $TRAVIS_COMMIT_RANGE
 then
   # Install tectonic using conda
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-  bash miniconda.sh -b -p `$HOME/miniconda
-  export PATH="$`HOME/miniconda/bin:`$PATH"
+  bash miniconda.sh -b -p $HOME/miniconda
+  export PATH="$HOME/miniconda/bin:$PATH"
   hash -r
   conda config --set always_yes yes --set changeps1 no
   conda update -q conda
@@ -21,10 +21,10 @@ then
   tectonic main.tex --print
   
   # Force push the paper to GitHub
-  cd $`TRAVIS_BUILD_DIR
-  git checkout --orphan `$TRAVIS_BRANCH-pdf
+  cd $TRAVIS_BUILD_DIR
+  git checkout --orphan $TRAVIS_BRANCH-pdf
   git rm -rf .
   git add -f ./main.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "building the cv"
-  git push -q -f https://$`GITHUB_USER:`$GITHUB_API_KEY@github.com/$`TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
+  git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
